6bd35d33acbb83ff97365600acea926d5aeab0eb
Veer1498 <singavarapuveerendra@gmail.com>
2022-10-12 02:16:02 +0530

    [Veer] Added : Create REST API

--- FirstSpringApp/src/main/java/com/firstspringapp/controller/HelloRestController.java
+++ FirstSpringApp/src/main/java/com/firstspringapp/controller/HelloRestController.java
@@ -1,36 +1,12 @@
 package com.firstspringapp.controller;
 
-import com.firstspringapp.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloRestController {
 
-    @RequestMapping(value = {"","/","/home"})
-    public String sayHello(){
+    @RequestMapping("/")
+    public String index() {
         return "Hello From BridgeLabz";
     }
-
-    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
-    public String sayHello(@RequestParam(value = "name") String name){
-        return "Hello" + name + "!";
-    }
-
-    @GetMapping("/param/{name}")
-    public String sayHelloParam(@PathVariable String name){
-        return "Hello" + name + "!";
-    }
-
-    @PostMapping("/post")
-    public String sayHello(@RequestBody User user){
-        return "Hello" + user.getFirstname() + user.getLastName() + "!";
-    }
-
-    @PutMapping("/put/{firstname}")
-    public String sayHello(@PathVariable String firstname,
-                           @RequestParam(value = "lastName") String lastName){
-        return "Hello" + firstname+ " "+lastName + "!";
-    }
-
 }
